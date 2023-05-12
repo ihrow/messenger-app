568ec6c2543ae8e8687e6d8cd8d4ad69a155989f
igor <igornotfound@gmail.com>
2023-05-12 13:12:07 +0200

    /users page + EmptyState component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,28 @@
-import ToasterContext from './context/ToasterContext'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import AuthContext from "./context/AuthContext";
+import ToasterContext from "./context/ToasterContext";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Messenger App',
-  description: 'A messenger app built with Next.js',
-}
+  title: "Messenger App",
+  description: "A messenger app built with Next.js",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToasterContext />
-        {children}</body>
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
+      </body>
     </html>
-  )
+  );
 }
