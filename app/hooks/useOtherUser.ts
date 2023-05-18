ad131def398eb91f8245c16cc20f4961a20b1588
ihrow <ihrow@proton.me>
2023-05-18 20:34:10 +0200

    useOtherUser fix

--- app/hooks/useOtherUser.ts
+++ app/hooks/useOtherUser.ts
@@ -1,22 +1,28 @@
 import { useSession } from "next-auth/react";
 import { useMemo } from "react";
 import { FullConversationType } from "../types";
-import { User } from '@prisma/client'
+import { User } from "@prisma/client";
 
-const useOtherUser = (conversation: FullConversationType | {
-  users: User[]
-}) => {
+const useOtherUser = (
+  conversation:
+    | FullConversationType
+    | {
+        users: User[];
+      }
+) => {
   const session = useSession();
 
   const otherUser = useMemo(() => {
     const currentUserEmail = session?.data?.user?.email;
 
-    const otherUser = conversation.users.filter((user) => user.email !== currentUserEmail)
+    const otherUser = conversation.users.filter(
+      (user) => user.email !== currentUserEmail
+    );
 
     return otherUser;
-  }, [session?.data?.user?.email, conversation.users])
+  }, [session?.data?.user?.email, conversation.users]);
 
-  return otherUser;
+  return otherUser[0];
 };
 
 export default useOtherUser;
